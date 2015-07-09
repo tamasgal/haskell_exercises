a66bba7ca6cdf3a84b3be81aea2d28b5f07d9f94
Tamas Gal <himself@tamasgal.com>
2015-07-10 00:27:27 +0200

    Solves problem 3

---
+++ ProjectEuler/p003.hs
@@ -0,0 +1,8 @@
+primeFactors :: Integer -> [Integer]
+primeFactors n =
+  case factors of
+    [] -> [n]
+    _  -> factors ++ primeFactors (n `div` (head factors))
+  where factors = take 1 $ filter (\x -> (n `mod` x) == 0) [2 .. n-1]
+
+answer = maximum $ primeFactors 600851475143
