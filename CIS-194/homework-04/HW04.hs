662ddd11952593af3dd752d128b0d780c7b036c1
Tamas Gal <himself@tamasgal.com>
2015-07-15 01:02:38 +0200

    Map as fold

--- CIS-194/homework-04/HW04.hs
+++ CIS-194/homework-04/HW04.hs
@@ -1,2 +1,16 @@
 xor :: [Bool] -> Bool
 xor = foldr (==) False . map (\n -> False) . filter (==True)
+
+
+fun1 :: [Integer] -> Integer
+fun1 [] = 1
+fun1 (x:xs)
+     | even x    = (x - 2) * fun1 xs
+     | otherwise = fun1 xs
+
+fun1' :: [Integer] -> Integer
+fun1' = product . map (\x -> x-2) . filter even
+
+
+map' :: (a -> b) -> [a] -> [b]
+map' f = foldr (\x y -> (f x):y) []
