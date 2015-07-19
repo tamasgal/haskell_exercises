2e563ac7e1608dd3b623e0212669f1ba61c53ba4
Tamas Gal <himself@tamasgal.com>
2015-07-19 16:42:08 +0200

    hugs!

--- ProjectEuler/p005.hs
+++ ProjectEuler/p005.hs
@@ -1,8 +1,1 @@
-dividible :: [Int] -> Int -> Bool
-dividible _ 0      = False
-dividible [] _     = True
-dividible (x:xs) n = dividibleBy n x && (dividible xs n)
-  where dividibleBy n x = n `mod` x == 0
-
-answer = head $ filter (dividible [1..20]) [0,20..]
-
+foldl (lcm) 1 [1..20]
