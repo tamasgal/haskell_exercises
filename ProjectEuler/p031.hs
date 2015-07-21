c43d5f158550ce95e2fad55b0c74403bc9045bd5
Tamas Gal <himself@tamasgal.com>
2015-07-21 13:57:24 +0200

    Solves problem 31

--- ProjectEuler/p031.hs
+++ ProjectEuler/p031.hs
@@ -1,6 +1,6 @@
-change :: [Integer] -> Integer -> Integer
+change :: [Int] -> Int -> Int
 change _ 0      = 1
-change [c] _    = 1
-change (c:cs) s = sum $ map (change cs . (s-)) [0,c..s]
+change [x] _    = 1
+change (x:xs) s = sum $ map (change xs . (s-)) [0,x..s]
 
 answer = change [200,100,50,20,10,5,2,1] 200
