ad6854c21bbc58a8954b24532b8e8c38ec54c0b6
Tamas Gal <himself@tamasgal.com>
2015-10-04 10:29:36 +0200

    Regex stuff

---
+++ Real_World_Haskell/ch08/Glob.hs
@@ -0,0 +1,11 @@
+module Glob (namesMatching) where
+
+import System.Directory (doesDirectoryExist, doesFileExist,
+                         getCurrentDirectory, getDirectoryContents)
+import System.FilePath (dropTrailingPathSeparator, splitFileName, (</>))
+import Control.Exception (handle)
+import Control.Monad (forM)
+import GlobRegex (matchesGlob)
+
+
+namesMatching = undefined
